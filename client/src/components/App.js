f2ef90e39c09be86fd3815bf8f7b5210c3b332a0
Masoud <masoud_mdar@yahoo.fr>
2021-05-25 22:44:51 +0200

    added puzzle

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,15 @@
 import React, {useState, useEffect} from "react"
+import axios from "axios"
+
 import Square from "./Square"
+
+import {BASE_URL} from "../utils/constants"
+import {puzzlesAndSolutions} from "../utils/puzzles"
 
 const App = () => {
 
     const [cellInput, setCellInput] = useState([])
+    const [selectedPuzzle, setSelectedPuzzle] = useState("")
 
     useEffect(() => {
         let tempArr = []
@@ -11,6 +17,10 @@ const App = () => {
             tempArr.push([])
         }
         setCellInput(tempArr)
+    }, [])
+
+    useEffect(() => {
+        setSelectedPuzzle(puzzlesAndSolutions[0][0])
     }, [])
 
     const handleClick = () => {
@@ -27,7 +37,7 @@ const App = () => {
             let tempArr = coordId.split("")
             tempArr.shift()
             const id = tempArr.join("")
-            console.log(id)
+            //console.log(id)
 
             let tempInputArr = []
 
@@ -40,12 +50,31 @@ const App = () => {
             let onePartValue = onePartValueArr.length > 1 ? onePartValueArr[onePartValueArr.length-1] : value
             tempInputArr[id].push(onePartValue)
 
+            let coordIdArr = coordId.split("")
+            if (coordIdArr.length > 2) {
+                coordIdArr.splice(1,2, (parseInt(id) - 8))
+
+            }
+
+            let finalCoordId = coordIdArr.join("")
+
+            let sendingData = {
+                puzzle: selectedPuzzle,
+                coordinate: finalCoordId,
+                value: onePartValue
+            }
+
+            axios.post(`${BASE_URL}/api/check`, sendingData).then(response => {
+                const {data} = response
+                console.log(data)
+            })
+
             setCellInput(tempInputArr)
 
         }
     }
 
-    //console.log(cellInput)
+    //console.log(selectedPuzzle)
 
 
     return (
@@ -53,7 +82,8 @@ const App = () => {
             <Square
                 data={{
                     handleChange: handleChange,
-                    cellInput: cellInput
+                    cellInput: cellInput,
+                    selectedPuzzle: selectedPuzzle
                 }}
             />
         </div>
--- client/src/components/Square.js
+++ client/src/components/Square.js
@@ -4,6 +4,13 @@ const Square = (props) => {
 
     let tableRow = ["A","B","C","D","E","F","G","H","I"]
 
+    const {selectedPuzzle} = props.data
+    //console.log(selectedPuzzle.length)
+    let selectedPuzzleArr = selectedPuzzle.split("")
+    let puzzleIndexArr = selectedPuzzleArr.map(element => {
+        return element === "." ? false : true
+    })
+
     const tableMaker = (row, d) => {
 
         let indexFinder = ((9*(d+1)) - 1)
@@ -13,7 +20,14 @@ const Square = (props) => {
         let n = indexFinder - 8
 
         while (n <= indexFinder) {
-            let item = <td key={Math.random() * Math.random()}><span><div className="warning"></div><input type="text" key={Math.random() * Math.random()} id={`${row}${n}`} onChange={props.data.handleChange} value={props.data.cellInput[n]} name="cell" className="cell"></input></span></td>
+            //console.log(n)
+            let item = <td key={Math.random() * Math.random()}>
+                <span>
+                    <div className="warning-row"></div><div className="warning-col"></div><div className="warning-reg"></div>
+                    <input type="text" key={Math.random() * Math.random()} id={`${row}${n}`} onChange={props.data.handleChange} value={puzzleIndexArr[n] ? selectedPuzzleArr[n] : props.data.cellInput[n]} name="cell" className="cell"></input>
+                </span>
+            </td>
+            
             tempArr.push(item)
             n++
         }
